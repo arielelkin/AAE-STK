b8c0a17e26b636a8e0cfcdfb31401a35cc846231
Ariel Elkin <ariel@arivibes.com>
2013-05-08 18:21:46 +0100

    initial commit

---
+++ AAE+STK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AAE+STK
+//
+//  Created by Ariel Elkin on 08/05/2013.
+//  Copyright (c) 2013 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AAE+STK/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AAE+STK
+//
+//  Created by Ariel Elkin on 08/05/2013.
+//  Copyright (c) 2013 ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
